1fec6a1add9a3cc935a122185da8bacf5d1b7a4d
Luke Elliott <revlucio@gmail.com>
2019-01-16 22:45:07 +0000

    attempting a recursive solution for problem 3

--- Euler/Euler/Problem3.fs
+++ Euler/Euler/Problem3.fs
@@ -40,3 +40,46 @@ let ``primes under`` () =
 let ``largest prime factor`` () =
     Assert.Equal(29L, largestPrimeFactor 131950L)
     
+    
+// take two!
+
+let rec calcFactors(n : int64, i : int64, max: int64, results: List<int64>) =
+    match i with 
+    | i when (i >= max) -> results
+    | i when (n % i = 0L) -> calcFactors(n, (i+1L), (n / i), i :: (n / i) :: results)
+    | i when (n % i <> 0L) -> calcFactors(n, (i+1L), (n / i), results)
+
+let factors(n : int64) = 
+    calcFactors(n, 1L, n, [])
+    |> List.distinct
+    |> List.sort
+
+[<Fact>]
+let ``find factors of`` () =
+    let result = factors 600851475143L
+    Assert.Equal<List<int64>>([1L; 2L], factors 2L)    
+    Assert.Equal<List<int64>>([1L; 3L], factors 3L)    
+    Assert.Equal<List<int64>>([1L; 2L; 4L], factors 4L)
+    Assert.Equal<List<int64>>([1L; 2L; 3L; 4L; 6L; 12L], factors 12L)
+    
+let rec calcIsPrime(n : int64, i : int64) =
+    match n with
+    | 1L -> false
+    | n when (i = n) -> true
+    | n when (n % i = 0L) -> false
+    | _ -> calcIsPrime(n, i+1L)
+    
+let isPrimeFast(n : int64) =
+    calcIsPrime(n, 2L)
+    
+[<Fact>]
+let ``is Prime fast`` () =
+    Assert.Equal(false, isPrimeFast(1L))
+    Assert.Equal(true, isPrimeFast(2L))
+    Assert.Equal(false, isPrimeFast(4L))
+    Assert.Equal(true, isPrimeFast(5L))
+    
+[<Fact>]
+let ``perf test of isPrimeFast`` () =
+    Assert.False(isPrimeFast 6008555L)
+    
