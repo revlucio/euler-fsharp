83cbf5ed5849dcbbbe8ff5ebbe1fae91db39e0aa
Luke Elliott <revlucio@gmail.com>
2019-01-19 09:58:12 +0000

    refactoring to use new methods

--- Euler/Euler/Problem3.fs
+++ Euler/Euler/Problem3.fs
@@ -3,12 +3,26 @@ module Problem3
 open System
 open Xunit
 
-let isPrime(x : int64) =
-    if (x = 1L) 
-    then false
-    else
-        ([2L..(x-1L)]
-        |> List.exists(fun y -> (x % y) = 0L)) = false
+let isPrimeFast(n : int64) =
+    let rec calcIsPrime(n : int64, i : int64) =
+        match n with
+        | 1L -> false
+        | n when (i = n) -> true
+        | n when (n % i = 0L) -> false
+        | _ -> calcIsPrime(n, i+1L)
+    calcIsPrime(n, 2L)
+
+let isPrime(x : int64) = isPrimeFast(x)
+
+let factors(n : int64) =
+    let rec calcFactors(n : int64, i : int64, max: int64, results: List<int64>) =
+        match i with 
+        | i when (i >= max) -> results
+        | i when (n % i = 0L) -> calcFactors(n, (i+1L), (n / i), i :: (n / i) :: results)
+        | i when (n % i <> 0L) -> calcFactors(n, (i+1L), (n / i), results) 
+    calcFactors(n, 1L, n, [])
+    |> List.distinct
+    |> List.sort
         
 let primesUnder (x : int64) =
     [1L..(x-1L)]
@@ -31,28 +45,10 @@ let ``is Prime`` () =
     Assert.Equal(true, isPrime(5L))
     Assert.Equal(true, isPrime(1009L))
     Assert.Equal(false, isPrime(1000000L))
-    
-[<Fact>]
-let ``primes under`` () =
-    Assert.Equal(1570, primesUnder(13195L).Length)
 
 [<Fact>]
-let ``largest prime factor`` () =
-    Assert.Equal(29L, largestPrimeFactor 131950L)
-    
-    
-// take two!
-
-let rec calcFactors(n : int64, i : int64, max: int64, results: List<int64>) =
-    match i with 
-    | i when (i >= max) -> results
-    | i when (n % i = 0L) -> calcFactors(n, (i+1L), (n / i), i :: (n / i) :: results)
-    | i when (n % i <> 0L) -> calcFactors(n, (i+1L), (n / i), results)
-
-let factors(n : int64) = 
-    calcFactors(n, 1L, n, [])
-    |> List.distinct
-    |> List.sort
+let ``perf test of isPrime`` () =
+    Assert.False(isPrime 600851475143L)
 
 [<Fact>]
 let ``find factors of`` () =
@@ -62,24 +58,10 @@ let ``find factors of`` () =
     Assert.Equal<List<int64>>([1L; 2L; 4L], factors 4L)
     Assert.Equal<List<int64>>([1L; 2L; 3L; 4L; 6L; 12L], factors 12L)
     
-let rec calcIsPrime(n : int64, i : int64) =
-    match n with
-    | 1L -> false
-    | n when (i = n) -> true
-    | n when (n % i = 0L) -> false
-    | _ -> calcIsPrime(n, i+1L)
-    
-let isPrimeFast(n : int64) =
-    calcIsPrime(n, 2L)
-    
 [<Fact>]
-let ``is Prime fast`` () =
-    Assert.Equal(false, isPrimeFast(1L))
-    Assert.Equal(true, isPrimeFast(2L))
-    Assert.Equal(false, isPrimeFast(4L))
-    Assert.Equal(true, isPrimeFast(5L))
+let ``largest prime factor`` () =
+    Assert.Equal(29L, largestPrimeFactor 13195L)
+
+
     
-[<Fact>]
-let ``perf test of isPrimeFast`` () =
-    Assert.False(isPrimeFast 6008555L)
     
