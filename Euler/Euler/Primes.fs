f0e4c1085106b86a5ce82cacc1b9522bdfb752f5
Luke Elliott <revlucio@gmail.com>
2019-01-23 22:20:33 +0000

    solved problem 10

--- Euler/Euler/Primes.fs
+++ Euler/Euler/Primes.fs
@@ -8,3 +8,12 @@ let isPrime(n : int64) =
         | n when (n % i = 0L) -> false
         | _ -> calc(n, i+1L)
     calc(n, 2L)
+    
+let isPrimex(n) =
+    let rec calc(n, i) =
+        match n with
+        | 1 -> false
+        | n when (i = n) -> true
+        | n when (n % i = 0) -> false
+        | _ -> calc(n, i+1)
+    calc(n, 2)
--- Euler/Euler/Problem010.fs
+++ Euler/Euler/Problem010.fs
@@ -1,21 +1,58 @@
 module Problem010
 
 open Xunit
-open Primes
 
-let sumOfAllPrimesUnder n = 
-    let rec loop sum i =
+let primesUnder n =
+    let rec setFactorsToFalse(list : array<bool>, i, n) =
+        if (i < list.Length) 
+        then 
+            list.[i] <- false
+            setFactorsToFalse(list, i+n, n)
+        else list
+ 
+    let rec loop(list : array<bool>, i) =
         match i with 
-        | i when (i >= n) -> sum
-        | i when (isPrime i) -> loop (sum+i) (i+2L)
-        | _ -> loop sum (i+2L)
-        
-    (loop 0L 1L) + 2L
+        | i when (i = list.Length) -> list
+        | 0 -> 
+            list.[i] <- false
+            loop(list, i+1)
+        | 1 -> 
+            list.[i] <- false
+            loop(list, i+1)
+        | i when (list.[i] = true) ->
+            loop(setFactorsToFalse(list, (i+i), i), i+1)
+        | _ ->
+            loop(list, i+1)
+    loop((Array.init n (fun _ -> true)), 0)
+
+let getLast(arr :array<bool>) = arr.[arr.Length-1]
 
 [<Fact>]
-let ``sum of all primes under 10`` () =
-    Assert.Equal(17L, (sumOfAllPrimesUnder 10L))
+let ``find primes under 5`` () =
+    Assert.Equal(false, getLast(primesUnder 2)) //1
+    Assert.Equal(true, getLast(primesUnder 3)) //2
+    Assert.Equal(true, getLast(primesUnder 4)) //3
+    Assert.Equal(false, getLast(primesUnder 5)) //4
+    Assert.Equal(true, getLast(primesUnder 6)) //5
+    Assert.Equal(false, getLast(primesUnder 10)) //9
+    Assert.Equal(false, getLast(primesUnder 52)) //51
+    Assert.Equal(true, getLast(primesUnder 1490)) //1489
+    
+let sumIndicies(arr : array<bool>) = 
+    let rec loop sum i =
+        match i with
+        | i when i = arr.Length -> sum
+        | i when arr.[i] -> loop (sum+int64(i)) (i+1)
+        | i when arr.[i] = false -> loop sum (i+1)
+        
+    loop 0L 0 
     
 [<Fact>]
-let ``sum of all primes under 100 000`` () =
-    Assert.Equal(454396537L, (sumOfAllPrimesUnder 100_000L)) //35.3 seconds to run
+let ``sum of indicies`` () = 
+    Assert.Equal(7L, sumIndicies([|false; true; true; false; true|]))
+    
+[<Fact>]
+let ``sum of primes under`` () =
+    Assert.Equal(17L, sumIndicies(primesUnder 10))
+    Assert.Equal(142913828922L, sumIndicies(primesUnder 2_000_000))
+    
